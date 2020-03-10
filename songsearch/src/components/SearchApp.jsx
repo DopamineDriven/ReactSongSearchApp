1fd883b2b7e82621a0d9ffe7ca06e5582886ccc5
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-10 07:53:57 -0500

    functions for results found interface

--- songsearch/src/components/SearchApp.jsx
+++ songsearch/src/components/SearchApp.jsx
@@ -5,7 +5,10 @@ import SearchForm from './SearchForm.jsx';
 export default class SearchApp extends React.Component {
     constructor() {
         super();
-
+        this.state = {
+            isLoading: false,
+            results: []
+        }
         //--------------------------------BINDING----------------------------------------
         this.makeSearch = this.makeSearch.bind(this)
     }
@@ -15,8 +18,33 @@ export default class SearchApp extends React.Component {
     // makeSearch method passed as prop on SearchForm component, onSearch
     // gives access to makeSearch method within SearchForm component
     // button click sends form info to makeSearch method
+    // http://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/#searchexamples
     makeSearch = (text) => {
-        fetch('https://itunes.apple.com/search?term=jack+johnson&entity=song')
+        this.setState({
+            isLoading: true
+        })
+        fetch(`https://itunes.apple.com/search?term=${text}&entity=song`)
+            .then((response) => {
+                if(response.status >= 200 && response.status < 300) {
+                    return response.json()
+                } else {
+                    throw new Error()
+                }
+            })
+            .then((json) => {
+                console.log(json)
+                this.setState({
+                    results: json.results
+                })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            .then(() => {
+                this.setState({
+                    isLoading: false
+                })
+            })
     }
 
     render() {
@@ -32,6 +60,12 @@ export default class SearchApp extends React.Component {
                     <SearchForm 
                         onSearch={this.makeSearch}
                     />
+                    {
+                        // loads how many results found below search form
+                        this.state.isLoading?
+                        `Loading...`:
+                        `${this.state.results.length} results found`
+                    }
                 </div>
                 <ul className="media-list">
                     <ResultItem />
